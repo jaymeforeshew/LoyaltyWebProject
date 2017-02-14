2755285d65e1ab30106865d3e996dbad9e139167
Jayme Foreshew <jrforeshew@gmail.com>
2017-02-14 11:25:13 -0500

    Fixing weird indentation

--- API/routes/api/tweets.js
+++ API/routes/api/tweets.js
@@ -1,5 +1,5 @@
 var express = require('express'),
-	router  = express.Router(),
+    router  = express.Router(),
     psql    = require('../../queries');
 
 router.get('/', function(req, res, next) {
@@ -7,15 +7,15 @@ router.get('/', function(req, res, next) {
 
 	return psql.any('SELECT name, tweetid, link, pointsgranted FROM twittertransactions JOIN shops USING(shopid) WHERE customerid=$1', [ customerID ]).then(function (data) {
 		var i,
-			returnObject = {},
-			returnData = [];
+		    returnObject = {},
+		    returnData = [];
 		
 		for (i=1; i <= data.length; i++) {
-			returnData.push({
-				type: 'tweets',
-				id: i,
-				attributes: data[i-1]
-			});
+		    returnData.push({
+		        type: 'tweets',
+		        id: i,
+			attributes: data[i-1]
+		    });
 		}
 		returnObject.data = returnData;
 
